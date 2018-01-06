2ce1fcd10203bc298698611768fce88490886d05
Steve Moore <lightenergystudios@iinet.net.au>
2018-01-06 17:52:50 +1000

    World tour cleanup

---
+++ Assets/WorldTourPanel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class WorldTourPanel : MonoBehaviour
+{
+	public delegate void WorldTourCongratsEndDelegate();
+	public WorldTourCongratsEndDelegate	OnWorldTourCongratsEnd;
+
+	// animation event
+	public void WorldTourCongratsEnd()
+	{
+		if (OnWorldTourCongratsEnd != null)
+			OnWorldTourCongratsEnd();
+	}
+}
